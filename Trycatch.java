3eb38a2923f1a525ac1115a2b4d033fa616ca377
nanaatt <emilzanatasya@gmail.com>
2024-10-01 12:30:13 +0700

    Update Trycatch.java

--- Trycatch.java
+++ Trycatch.java
@@ -4,22 +4,21 @@
  */
 package trycatch;
 
-import java.util.Scanner;
 
 /**
  *
  * @author Careen Emilza
  */
 public class Trycatch {
-    
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        try{
-            System.out.println("Masukkan id pelanggan: ");
-            int contoh = sc.nextInt();
-        }catch (Exception e){
-            System.out.println("Id pelanggan hanya berupa angka.");
+        int pembilang = 10;
+        int penyebut = 0;
+
+        try {
+            int hasil = pembilang / penyebut;
+            System.out.println("Hasil bagi: " + hasil);
+        } catch (ArithmeticException e) {
+            System.out.println("Terjadi kesalahan: Pembagian dengan nol tidak diperbolehkan.");
         }
     }
-    
 }
